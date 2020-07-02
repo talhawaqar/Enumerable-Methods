e2650610f881956be1f1e36b83a649b6c0b174c2
talhawaqar <talhawaqar007@hotmail.com>
2020-07-02 18:46:10 +0500

    Fix some rubocop errors

--- enumerables.rb
+++ enumerables.rb
@@ -1,9 +1,10 @@
 module Enumerable
   def my_each
     return to_enum unless block_given?
-    returned_array = [] 
+
+    returned_array = []
     i = 0
-    while i < length do
+    while i < length
       returned_array << self[i]
       yield(self[i])
       i += 1
@@ -11,13 +12,14 @@ module Enumerable
     returned_array
   end
 
-  def my_each_with_index 
+  def my_each_with_index
     return to_enum unless block_given?
-    returned_array = [] 
+
+    returned_array = []
     i = 0
-    while i < length do
+    while i < length
       returned_array << self[i]
-      yield(self[i],i)
+      yield(self[i], i)
       i += 1
     end
     returned_array
@@ -25,17 +27,17 @@ module Enumerable
 
   def my_select
     return to_enum unless block_given?
+
     returned_array = []
     self.my_each do |n|
-      if yield(n)
-        returned_array << n
-      end
+      returned_array << n if yield(n)
     end
     returned_array
   end
 
   def my_all?
     return to_enum unless block_given?
+
     returned_value = false
     self.my_each do |n|
       if yield(n)
@@ -47,9 +49,10 @@ module Enumerable
     end
     returned_value
   end
-  
+
   def my_any?
     return to_enum unless block_given?
+
     returned_value = false
     self.my_each do |n|
       if yield(n)
@@ -62,6 +65,7 @@ module Enumerable
 
   def my_none?
     return to_enum unless block_given?
+
     returned_value = true
     self.my_each do |n|
       if yield(n)
@@ -70,51 +74,49 @@ module Enumerable
       end
     end
     returned_value
-  end 
+  end
 
   def my_count
     return length unless block_given?
+
     returned_value = 0
     self.my_each do |n|
-      if yield(n)
-        returned_value += 1
-      end
+      returned_value += 1 if yield(n)
     end
     returned_value
   end
 
   def my_map
     return to_enum unless block_given?
-    returned_array = [] 
+
+    returned_array = []
     i = 0
-    while i < length do
+    while i < length
       returned_array << yield(self[i])
       i += 1
     end
     returned_array
   end
 
-  def my_inject(accumulator=nil)
+  def my_inject(accumulator = nil)
     return to_enum unless block_given?
-    
-    if accumulator.nil?
-      accumulator = 0
-    end
-    
-    i=0
-    while i < length do
-      accumulator = yield(accumulator,self[i])
+
+    accumulator = 0 if accumulator.nil?
+    i = 0
+    while i < length
+      accumulator = yield(accumulator, self[i])
       i += 1
     end
     accumulator
   end
 end
 
+test_array = [2, 5, 7]
+# r = test_array.my_select {|n| n.even?}
+# print r
+# Function tu test my_inject
 def multiply_els(my_array)
   my_array.my_inject(1) { |multiply, number| multiply * number }
 end
-
-test_array = [2, 5, 7]  
-
 multiply = multiply_els(test_array)
 print multiply
