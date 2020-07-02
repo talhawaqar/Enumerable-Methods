dc0b213e70a10cf756a007953c7b07e8ada2e1f9
talhawaqar <talhawaqar007@hotmail.com>
2020-07-02 14:22:56 +0500

    Add my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -1,17 +1,29 @@
 module Enumerable
-    def my_each
-      return to_enum unless block_given?
-      returned_array = [] 
-      i = 0
-      while i < length do
-        returned_array << self[i]
-        yield(self[i])
-        i += 1
-      end
-      returned_array
+  def my_each
+    return to_enum unless block_given?
+    returned_array = [] 
+    i = 0
+    while i < length do
+      returned_array << self[i]
+      yield(self[i])
+      i += 1
     end
+    returned_array
   end
-  array = [1, 2, 3]
-  
-d = array.my_each {|x| puts x}
-print d
+
+  def my_each_with_index 
+    return to_enum unless block_given?
+    returned_array = [] 
+    i = 0
+    while i < length do
+      returned_array << self[i]
+      yield(self[i],i)
+      i += 1
+    end
+    returned_array
+  end
+end
+
+array = [1, 2, 3]  
+a = array.my_each_with_index { |value,index| print "#{index}#{value} \n" }
+print "\n  #{a}\n"
