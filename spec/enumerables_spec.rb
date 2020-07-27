3de650f58ecbc23c5ccf3aba4ffdfdca9265ea2d
rumbie <rumbiemudzie@gmail.com>
2020-07-27 15:38:08 +0200

    Add test cases for my_each_with_index and my_select

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -20,5 +20,49 @@ describe Enumerable do
     it 'it iterates through hash and it returns hash' do
       expect(my_hash.my_each { |x| x }).to eq({ a: 1, b: 2 })
     end
+
+
+    it 'calculate sum for each element of an array' do
+      sum = 0 
+      my_array.my_each { |x| sum = x + sum}
+      expect(sum).to eq(15)
+    end
+    
   end
+
+  describe '#my_each_with_index' do
+    it 'iterates to entire array of numbers and return array' do
+      expect(my_array.my_each_with_index { |x,y| x }).to eq([1, 2, 3, 4, 5])
+    end
+
+    it 'iterates to range of numbers and return range' do
+      expect((1..5).my_each_with_index { |x,y| x }).to eq(1..5)
+    end
+
+    it 'it returns enumerable when no block is passed' do
+      expect((1..5).my_each_with_index).to be_a Enumerable
+    end
+
+    it 'it iterates through hash and it returns hash' do
+      expect(my_hash.my_each_with_index { |x,y| x }).to eq({ a: 1, b: 2 })
+    end
+
+    it 'calculate sum for each element of an array' do
+      sum = 0 
+      my_array.my_each_with_index { |x,y| sum = y + sum}
+      expect(sum).to eq(10)
+    end
+  end
+
+  describe '#my_select' do
+    it 'selects an item in the array and returns true' do
+      expect(my_array.my_select { |x| x > 3}).to eq([4,5])
+    end
+
+    it 'returns enumerable when no block is given' do
+      expect(my_array.my_select).to be_a Enumerable
+    end
+  end
+
+
 end
